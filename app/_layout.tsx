4e0a95153c9d6e03d2caef55da4ccf7692595889
Jonathan Freed <jon.freed@gmail.com>
2025-10-26 14:33:27 +0700

    clean up 1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,19 +4,13 @@ import {
   DefaultTheme as NavigationDefaultTheme,
   ThemeProvider,
 } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { GestureHandlerRootView } from 'react-native-gesture-handler'; // Import the wrapper
 
 import { useColorScheme } from '@/app/hooks/useColorScheme';
 import { Colors } from '@/app/constants/Colors';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 const CustomDarkTheme = {
   ...NavigationDarkTheme,
@@ -40,20 +34,6 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const selectedTheme =
     colorScheme === 'dark' ? CustomDarkTheme : CustomLightTheme;
-
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
